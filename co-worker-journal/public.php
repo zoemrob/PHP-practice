917a82c89476bc480d17b9836640103f461084d4
Zoe Robertson <zoe.robertson@getalma.com>
2018-02-05 16:08:05 -0800

    started basic framework for public webpage

---
+++ co-worker-journal/public.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<?php include(ClassesPractice.php); ?>	
+	</head>
+	<body>
+		<div class="header">
+			<h1>Co-Worker Journal</h1>
+		</div>
+		<div class="name">
+			<span>Person's Name</span>
+		</div>
+		<div class="gender">
+			<p>Gender:</p>
+		</div>
+		<div class="age">
+			<p>Age: </p>
+		</div>
+		<div class="event-log">
+			<ul>
+				<li>Item 1</li>
+				<li>Item 2</li>
+				<li>Item 3</li>
+				<li>Item 4</li>
+				<li>Item 5</li>
+			</ul>
+		</div>
+	</body>
+</html>

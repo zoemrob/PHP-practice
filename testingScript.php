2b14daa4e88d4034939503d57bfa10b4fa3afd29
Zoe Robertson <zoe.robertson.m@gmail.com>
2018-02-11 19:00:00 -0800

    Finally cleaned up branches... learned my lesson on committing regularly :)

--- testingScript.php
+++ testingScript.php
@@ -1,8 +1,6 @@
 <?php
 // Testing script, loads in various data, runs tests on it for debugging. 
 require('PersonClass.php');
-require('vendor/autoload.php');
-require('MongoHelper.php');
 $person = new BasePerson('5a7fe017e662dc7ec495262d');
 $person->getName();
 $person->getAge();
@@ -11,7 +9,6 @@ $person->getSex();
 //var_dump($person->getNotes());
 
 /*$person->debugNotes();
->>>>>>> Stashed changes
 $person->setNote('Here is the second note that should be loaded.');
 $person->setNote('Here is the third note that should be loaded.');
 $person->setNote('Here is the fourth note that should be loaded.');
@@ -22,29 +19,7 @@ $person->deleteNotes([1, 3]);
 $person->debugNotes();
 $person->deleteNotes([0]);
 $person->debugNotes();
+<<<<<<< Updated upstream
 $person->deleteNotes([], true);
 <<<<<<< Updated upstream
-=======
-*/
-
-//database testing 
-
-		/*$db = MongoHelper::createDBInstance();
-		$resultOfQuery = MongoHelper::queryById($db, "5a7fe017e662dc7ec495262d");
-		$name = MongoHelper::getDocName($resultOfQuery); 
-		echo $name . "\n";
-*/	
-
-
-/*
-print_r(MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($resultOfQuery)));
-
-$letsHope = $resultOfQuery->jsonSerialize();
-foreach($letsHope as $element) {
-	echo $element . "\n";
-}
-print_r($letsHope);
-
-echo $letsHope->_id . "\n";
-echo $letsHope->firstName . "\n";
 */

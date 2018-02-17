af6e5758506222bcc8cdd7f344ab3dc30264f536
Zoe Robertson <zoe.robertson.m@gmail.com>
2018-02-17 15:27:29 -0800

    added rough button for new entry, rough form handler

--- personInstanceFormHandler.php
+++ personInstanceFormHandler.php
@@ -1,3 +1,6 @@
 <?php
 require('NewEntryClass.php');
 require('PersonClass.php');
+
+$receivedData = file_get_contents('php://input');
+HelperClass::generateNewEntryForm();
--- public.php
+++ public.php
@@ -9,14 +9,14 @@
 		<?php 
 		require("PersonClass.php");
 		$person = new BasePerson("5a7fe017e662dc7ec495262d"); 
-		/*$person->setNote("I met up with some of my peers in the Udacity Grow-with-Google Scholarship. We had a great time, we learned a lot. I arrived first at Case Study Coffee Roasters. Ginny arrived shortly after. We finished studying there, then we went to Starbucks, and studied a bit more. Andrew showed up a bit later, and now we are getting ready to go over a group project");
-		$person->setNote("This is your 2nd test note.");
-		$person->setNote("This is your 3rd test note.");*/
 		?>
 	</head>
 	<body>
 		<div class="header">
 			<h1 class="header-text">Co-Worker Journal</h1>
+		</div>
+		<div class="new-entry-div">
+			<button id="new-entry-button" type="button">New Entry</button>
 		</div>
 		<div class="container">
 			<div class="demographics">

e646ef6cf2f78d9c3840a022bf2f4a06fa2bcabb
Zoe Robertson <zoe.robertson@getalma.com>
2018-02-16 16:24:31 -0800

    fixed a couple method names, added initial comments to NewEntry

--- NewEntryClass.php
+++ NewEntryClass.php
@@ -10,7 +10,7 @@ Class NewEntry extends AbstractPerson {
 	private $lastName;
 
 	public function __construct($JSONString) {
-		$this->getReceivedData($JSONString);
+		$this->setReceivedData($JSONString);
 		$this->setName();
 		$this->setAge();
 		$this->setSex();
@@ -21,29 +21,35 @@ Class NewEntry extends AbstractPerson {
 		return $mongoId;
 	}
 
+	// this method sets the $firstName, $lastName, and $name properties from the $JSONString arg passed to the constructor.
 	public function setName() {
 		$this->firstName = $this->receivedData['firstName'];
 		$this->lastName = $this->receivedData['lastName'];
 		$this->name = $this->firstName . " " . $this->lastName;
 	}
 
+	// this method sets the $age property from the $JSONString arg passed to the constructor.
 	public function setAge() {
 		$this->age = $this->receivedData['age'];
 	}
 
+	// this method sets the $sex property from the $JSONString arg passed to the constructor.
 	public function setSex() {
 		$this->sex = $this->receivedData['sex'];
 	}
 
-	private function getReceivedData($JSONString) {
+	// this method formats the $JSONString into a usable array for the object, and sets the $receivedData property.
+	private function setReceivedData($JSONString) {
 		$this->receivedData = HelperClass::JSONtoArray($JSONString);
 	}
 
+	// this method allows for a method value/name to be passed into the constructor. For future use after sanitizing input.
 	private function executeMethod($method) {
 		$this->$method();
 	}
 
-	public function insertNoteIntoDB($firstName, $lastName, $sex, $age) {
+	// this method allows for the $JSONString data to be set used to add a new entry to the database collection.
+	public function insertEntryIntoDB($firstName, $lastName, $sex, $age) {
 		echo MongoHelper::insertNewEntryIntoDB($this->getCollection(), $this->firstName, $this->lastName, $this->sex, $this->age);
 	}
 }

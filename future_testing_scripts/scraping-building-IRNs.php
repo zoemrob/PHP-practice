cdf9e302f15797dfda8f10df98abb42f76349af7
Zoe Robertson <zoe.robertson@getalma.com>
2018-03-02 16:53:05 -0800

    added comments to building scraper

--- future_testing_scripts/scraping-building-IRNs.php
+++ future_testing_scripts/scraping-building-IRNs.php
@@ -1,4 +1,107 @@
-<?php
+<?php 
+$counties = array (
+"Adams",
+"Allen",
+"Ashland",
+"Ashtabula",
+"Athens",
+"Auglaize",
+"Belmont",
+"Brown",
+"Butler",
+"Carroll",
+"Champaign",
+"Clark",
+"Clermont",
+"Clinton",
+"Columbiana",
+"Coshocton",
+"Crawford",
+"Cuyahoga",
+"Darke",
+"Defiance",
+"Delaware",
+"Erie",
+"Fairfield",
+"Fayette",
+"Franklin",
+"Fulton",
+"Gallia",
+"Geauga",
+"Greene",
+"Guernsey",
+"Hamilton",
+"Hancock",
+"Hardin",
+"Harrison",
+"Henry",
+"Highland",
+"Hocking",
+"Holmes",
+"Huron",
+"Jackson",
+"Jefferson",
+"Knox",
+"Lake",
+"Lawrence",
+"Licking",
+"Logan",
+"Lorain",
+"Lucas",
+"Madison",
+"Mahoning",
+"Marion",
+"Medina",
+"Meigs",
+"Mercer",
+"Miami",
+"Monroe",
+"Montgomery",
+"Morgan",
+"Morrow",
+"Muskingum",
+"Noble",
+"Ottawa",
+"Paulding",
+"Perry",
+"Pickaway",
+"Pike",
+"Portage",
+"Preble",
+"Putnam",
+"Richland",
+"Ross",
+"Sandusky",
+"Scioto",
+"Seneca",
+"Shelby",
+"Stark",
+"Summit",
+"Trumbull",
+"Tuscarawas",
+"Union",
+"Van",
+"Wert",
+"Vinton",
+"Warren",
+"Washington",
+"Wayne",
+"Williams",
+"Wood");
+
+$countyAndNumber = array();
+$i = 1;
+foreach($counties as $county) {
+	if (strlen($i) == 1) {
+		$alteredI = '0' . $i;
+		$countyAndNumber[$alteredI] = $county;
+		$i++;
+	} else {
+		$countyAndNumber["{$i}"] = $county;
+		$i++;
+	}
+}
+
 $formatted = '';
 //for ($i = 1; $i < 88; $i++) {
 	/*if (strlen($i) == 1) {
@@ -22,3 +125,7 @@ $formatted = '';
 echo $formatted;
 
 //file_put_contents('codes.txt', $formatted);
+'Jefferson High School (9-12)                 IRN:   018184'
+
+'<FONT COLOR="A62A2A">                                                                                                            
+Jefferson High School (9-12)                 IRN:   018184' //188 characters
